81546039ab498c7deafb9d591c7091138b372587
AnyaGW <a.bogoslovskaja@yandex.ru>
2026-02-05 21:13:05 +0500

    добавлен гамбургер для меню на мобильном

--- src/els/@header/index.tsx
+++ src/els/@header/index.tsx
@@ -1,6 +1,7 @@
 import type { ReactElement } from 'react';
 import s from './styles.module.css';
 import { Logo } from '../logo';
+import { cn } from '../@common/cn';
 
 export const Header = (): ReactElement => {
 	return (
@@ -8,6 +9,9 @@ export const Header = (): ReactElement => {
 			<div className={s.topPart}>
 				<div className="container">
 					<Logo />
+					<button className={cn(s.menuToggle)}>
+						<span className={s.hamburger}></span>
+					</button>
 				</div>
 			</div>
 		</header>
