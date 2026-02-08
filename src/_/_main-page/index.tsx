fea0130daf58c6321c4202791028f215051eb806
AnyaGW <a.bogoslovskaja@yandex.ru>
2026-02-08 15:08:32 +0500

    Добавлен остальные 3 блока про системы.

--- src/_/_main-page/index.tsx
+++ src/_/_main-page/index.tsx
@@ -3,6 +3,9 @@ import surveillance from './img/surveillance-1.jpg';
 import fitters from './img/fitters.avif';
 import cable from './img/cable-1.jpg';
 import access from './img/access-1.jpg';
+import signaling from './img/signaling-1.jpg';
+import notification from './img/notification-1.jpg';
+import vcs from './img/vcs-1.jpg';
 import s from './styles.module.css';
 
 export function MainPage() {
@@ -23,7 +26,7 @@ export function MainPage() {
 					<span className={s.helpHeadline}>ПОМОЖЕМ</span>
 					<span className={s.helpDescr}>
 						Разработать и реализовать комплексные проекты для предприятий и
-						организаций любого масштаба. \ Главная цель – создание у наших
+						организаций любого масштаба. Главная цель – создание у наших
 						партнеров современной и надежной инфраструктуры по безопасности и
 						связи.
 					</span>
@@ -32,6 +35,9 @@ export function MainPage() {
 					<System imageSrc={surveillance} name="Видеонаблюдение" link="" />
 					<System imageSrc={cable} name="Кабельные системы" link="" />
 					<System imageSrc={access} name="Контроль доступа" link="" />
+					<System imageSrc={signaling} name="Сигнализация" link="" />
+					<System imageSrc={notification} name="Оповещение" link="" />
+					<System imageSrc={vcs} name="Система ВКС" link="" />
 				</div>
 			</div>
 			<div className={s.partners}>
