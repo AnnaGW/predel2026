bdbbca4b5e49fcee63e6016a56468d467631fdb7
AnyaGW <a.bogoslovskaja@yandex.ru>
2026-02-11 00:34:22 +0500

    Добавлена страница 404

--- src/_/_error404/error404.tsx
+++ src/_/_error404/error404.tsx
@@ -1,9 +1,21 @@
+import { Block } from '~/els/block';
+import s from './styles.module.css';
+import { AppRoute } from '~/const';
+
 function Error404() {
 	return (
-		<>
-			<h1>404</h1>
-			<h2>Ничего не нашлось</h2>
-		</>
+		<div className={s.mainLayout}>
+			<div className={s.bgFixed}></div>
+			<div className="container">
+				<Block>
+					<h1>404</h1>
+					<h2>К сожалению, страница не найдена</h2>
+					<a className={s.link} href={AppRoute.MainPage}>
+						На главную
+					</a>
+				</Block>
+			</div>
+		</div>
 	);
 }
 
