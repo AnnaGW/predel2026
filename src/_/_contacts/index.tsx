59ae43c3499cb35b40f8e989afe0132a56d12b02
AnyaGW <a.bogoslovskaja@yandex.ru>
2026-02-21 21:09:41 +0500

    улучшены стили страницы Контакты

--- src/_/_contacts/index.tsx
+++ src/_/_contacts/index.tsx
@@ -12,6 +12,9 @@ export function ContactsPage() {
 						450074 г. Уфа, бульвар Н. Наджми, д. 19, офис 2А info@predelufa.ru
 					</p>
 					<p>Телефон: +7 (347) 262-91-75 +7 (937) 849-91-75</p>
+					<p className={s.contactsCard}>
+						<a href={'files/Predel_partner_card.doc'}>Карта партнера</a>
+					</p>
 				</div>
 				<div className={s.contactsBorder}></div>
 				<div className={s.contactsDetails}>
@@ -21,14 +24,15 @@ export function ContactsPage() {
 						0278921967 КПП 027401001 ОГРН 1160280112849
 					</p>
 					<p>
-						БАШКИРСКОЕ ОТДЕЛЕНИЕ N8598 ПАО СБЕРБАНК Кор. сч.
-						30101810300000000601 БИК 048073601 Расч. сч. 40702810106000020026
+						БАШКИРСКОЕ ОТДЕЛЕНИЕ N8598 ПАО СБЕРБАНК <br />
+						Кор. сч. 30101810300000000601 БИК 048073601 <br />
+						Расч. сч. 40702810106000020026 <br />
 					</p>
 					<p>Директор Мараховский Павел Валерьевич</p>
 				</div>
-				<p className={s.contactsCard}>
+				{/* <p className={s.contactsCard}>
 					<a href={'files/Predel_partner_card.doc'}>Карта партнера</a>
-				</p>
+				</p> */}
 			</div>
 			<iframe
 				src="https://yandex.ru/map-widget/v1/?um=constructor%3A80bfeabf7d1eb5332eca31227a85813c78d28f1e9b9f1de2e439a9564058771c&amp;source=constructor"
