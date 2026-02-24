1bb8077be9c151a62c2f03a7bd9e952f9b61a9b4
AnyaGW <a.bogoslovskaja@yandex.ru>
2026-02-24 17:16:30 +0500

    проверка режима vite.

--- src/_/index.tsx
+++ src/_/index.tsx
@@ -2,8 +2,11 @@ import { Outlet } from 'react-router-dom';
 import s from './styles.module.css';
 import { Header } from '~/els/@header';
 import { Footer } from '~/els/@footer';
+import { AppRoute, getBase } from '~/const';
 
 function MainLayout() {
+	console.log('MODE is ', import.meta.env.MODE);
+	console.log('ROUTE is ', getBase(AppRoute.MainPage));
 	return (
 		<div className={s.mainLayout}>
 			<div className={s.bgFixed}></div>
