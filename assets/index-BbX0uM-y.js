8dfd98b089821fd02285f7063eae3b3294bd4cb3
AnyaGW <a.bogoslovskaja@yandex.ru>
2026-02-27 19:43:00 +0500

    Updates

--- assets/index-BbX0uM-y.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-CCo3pIpG.js";const s="_headline_g9jkv_1",a={headline:s},t=({text:e})=>n.jsx("h1",{className:a.headline,children:e});export{t as H};
---
+++ assets/index-C1vmAWzi.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-Dj_tPo7B.js";const s="_headline_g9jkv_1",a={headline:s},t=({text:e})=>n.jsx("h1",{className:a.headline,children:e});export{t as H};
